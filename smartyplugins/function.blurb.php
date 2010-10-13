6deff590955a68f4f3c288ae117c110eb8219382
Will James <will@tekimaki.com>
2010-10-13 17:45:49 -0400

    update to display edit link for non-existant blurb, and some other cleanup.

--- smartyplugins/function.blurb.php
+++ smartyplugins/function.blurb.php
@@ -19,7 +19,7 @@
  */ 
 
 function smarty_function_blurb( $pParams, &$gBitSmarty ) {
-	require_once('function.biticon.php');
+	require_once( 'function.biticon.php' );
 	require_once( BLURB_PKG_PATH.'BitBlurb.php' );
 	// at a minimum, return blank string (not empty) so we still replace the tag
 	$ret = ' ';
@@ -28,32 +28,41 @@ function smarty_function_blurb( $pParams, &$gBitSmarty ) {
 	if( empty( $pParams['blurb_guid'] )) {
 		return $ret;
 	}
-	//Load up the blurb class
-	$BitBlurb  = new BitBlurb( );
 	
+	$blurb_id = NULL;
+
+	// wrap blurb in a div
+	$divOpen .= '<div class="blurb '.$pParams['blurb_guid'].'">';
+	$divClose .= "</div>";
+
 	//Grab the blurb id from the database. If not exist, return error
-	$blurb_id = BitBlurb::getIdByField('blurb_guid',$pParams['blurb_guid']);
-	if( empty( $blurb_id ) ) {
+	if( !($blurb_id = BitBlurb::getIdByField('blurb_guid',$pParams['blurb_guid']) ) ){
 		@error_log( tra( "'No blurb found with the name: ')".$pParams['blurb_guid'] ) );
-		$ret = "FAIL";
-		return $ret;
-	} else {
-		//Set the class variable blurb id and load up the contents
-		$BitBlurb->mBlurbId = $blurb_id;
+		$BitBlurb  = new BitBlurb();
+		if( $BitBlurb->hasUpdatePermission() || $BitBlurb->hasCreatePermission() ){
+			$ret = $divOpen;
+			$ret .="<a href=\"".BLURB_PKG_URL."edit_blurb.php?blurb_guid=".$pParams['blurb_guid']."\" >".smarty_function_biticon(array('ipackage'=>'icons', 'iname'=>'accessories-text-editor', 'iexplain'=>'Create Blurb:'.$pParams['blurb_guid']),$gBitSmarty)."</a>";
+			$ret .= $divClose;
+		}
+	}else{
+		// open the div
+		$ret = $divOpen;
+		//Load up the blurb class
+		$BitBlurb  = new BitBlurb( $blurb_id );
 		$BitBlurb ->load();
+		
+		//Check if title should be displayed and assign it appropriatly
+		if( !empty($pParams['title']) ) {
+			$ret .= "<h3>".$BitBlurb->mInfo['title']."</h3>";
+		}
+		//Extract the blurb
+		$ret .= $BitBlurb ->mInfo['data'];
+		// Allow user to edit blurb if they have permission to do so
+		if( $BitBlurb->isValid() && ($BitBlurb->hasUpdatePermission() || $BitBlurb->hasCreatePermission())){
+			$ret .="<a href=\"".BLURB_PKG_URL."edit_blurb.php?blurb_id=".$blurb_id."\" >".smarty_function_biticon(array('ipackage'=>'icons', 'iname'=>'accessories-text-editor', 'iexplain'=>'Edit'.$pParams['blurb_guid']),$gBitSmarty)."</a>";
+		}
+		// close the div
+		$ret .= $divClose;
 	}
-	
-	//Check if title should be displayed and assign it appropriatly
-	$ret .= '<div class="blurb" id="'.$pParams['blurb_guid'].'">';
-	if( !empty($pParams['title']) ) {
-		$ret .= "<h3>".$BitBlurb ->mInfo['title']."</h3>";
-	}
-	//Extract the blurb
-	$ret .= $BitBlurb ->mInfo['data'];
-	// Allow user to edit blurb if they have permission to do so
-	if( $BitBlurb->isValid() && ($BitBlurb->verifyUpdatePermission() || $BitBlurb->verifyCreatePermission())){
-		$ret .="<a href=\"".BLURB_PKG_URL."edit_blurb.php?blurb_id=".$blurb_id."\" >".smarty_function_biticon(array('ipackage'=>'icons', 'iname'=>'accessories-text-editor', 'iexplain'=>'Edit'.$pParams['blurb_guid']),$gBitSmarty)."</a>";
-	}
-	$ret .= "</div>";
 	return $ret;
 }
