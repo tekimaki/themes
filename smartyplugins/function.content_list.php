877dd4e88c1cc9fe47f65f97b7c3254eb18c7673
tekimaki <will@tekimaki.com>
2010-10-29 12:19:20 -0400

    param remapping to vars only needed for center list inc

--- smartyplugins/function.content_list.php
+++ smartyplugins/function.content_list.php
@@ -12,15 +12,6 @@ function smarty_function_content_list( $pParams, &$gBitSmarty ) {
 	// at a minimum, return blank string (not empty) so we still replace the tag
 	$ret = ' ';
 
-	// some content specific offsets and pagination settings
-	if( !empty( $pParams['sort_mode'] )) {
-		$content_sort_mode = $pParams['sort_mode'];
-	}
-
-	$max_content = ( !empty( $pParams['max_records'] )) ? $pParams['max_records'] : $gBitSystem->getConfig( 'max_records',10 );
-
-	$cListRequest = $pParams;
-
 	if( !empty( $pParams['content_type_guid'] ) && !empty( $gLibertySystem->mContentTypes[$pParams['content_type_guid']] ) ){
 		$pParam['sort_mode'] = !empty( $pParams['sort_mode'] )?$pParams['sort_mode']:'last_modified';
 		$class = $gLibertySystem->mContentTypes[$pParams['content_type_guid']]['handler_class'];
@@ -33,6 +24,15 @@ function smarty_function_content_list( $pParams, &$gBitSmarty ) {
 		}
 	}
 	else{
+		// some content specific offsets and pagination settings
+		if( !empty( $pParams['sort_mode'] )) {
+			$content_sort_mode = $pParams['sort_mode'];
+		}
+
+		$max_content = ( !empty( $pParams['max_records'] )) ? $pParams['max_records'] : $gBitSystem->getConfig( 'max_records',10 );
+
+		$cListRequest = $pParams;
+
 		// now that we have all the offsets, we can get the content list
 		include_once( LIBERTY_PKG_PATH.'get_content_list_inc.php' );
 	}
